7c5cae506c4998a0adad24208965db7340474ef2
xiao-san <xiaosangaming88@gmail.com>
2023-12-28 01:42:57 +0545

    modified player to fit on screen width

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import 'dart:async';
 import 'package:brick_breaker/coverscreen.dart';
+import 'package:flutter/services.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -33,24 +34,67 @@ class _HomepageState extends State<Homepage> {
     });
   }
 
+  //move player left
+  void moveLeft() {
+    setState(() {
+      if(!(playerX <= -1 )) {
+       playerX -= 0.2;
+      }
+    });
+  }
+
+  //move player right
+  void moveRight() {
+    setState(() {
+      if(!(playerX >=1 )) {
+       playerX += 0.2;
+      }
+      
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.deepPurple[100],
-        body: Stack(
-          children: [
-            //tap to play
-            CoverScreen(
-              hasGameStarted: hasGameStarted,
-              onStart: startGame,
-            ),
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      autofocus: true,
+      onKey: (value) {
+        if (value.isKeyPressed(LogicalKeyboardKey.arrowLeft)) {
+          moveLeft();
+        } else if (value.isKeyPressed(LogicalKeyboardKey.arrowRight)) {
+          moveRight();
+        }
+      },
+      child: Scaffold(
+          backgroundColor: Colors.deepPurple[100],
+          body: Stack(
+            children: [
+              //tap to play
+              CoverScreen(
+                hasGameStarted: hasGameStarted,
+                onStart: startGame,
+              ),
 
-            //ball
-            MyBall(ballX: ballX, ballY: ballY),
+              //ball
+              MyBall(ballX: ballX, ballY: ballY),
 
-            //player 
-            MyPlayer(playerX: playerX, playerWidth: playerWidth,)
-          ],
-        ));
+              //player
+              MyPlayer(
+                playerX: playerX,
+                playerWidth: playerWidth,
+              ),
+
+              //where playerX is ?
+              Container(
+                alignment: Alignment(playerX, 0.9),
+                child: Container(
+                  height: 15,
+                  width: 4,
+                  color: Colors.red,
+                ),
+              )
+            ],
+          )),
+    );
   }
 }
--- lib/player.dart
+++ lib/player.dart
@@ -9,10 +9,13 @@ class MyPlayer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment(playerX, 0.9),
-      child: Container(
-        height: 10,
-        width: MediaQuery.of(context).size.width * playerWidth / 2,
-        color: Colors.deepPurple,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          height: 10,
+          width: MediaQuery.of(context).size.width * playerWidth / 2,
+          color: Colors.deepPurple,
+        ),
       ),
     );
   }
